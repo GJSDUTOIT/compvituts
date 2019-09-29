c34d253e7ec13bf976b6f37dec72f909d5c242e0
GJSDTDELL <20277970@sun.ac.za>
2019-09-30 00:56:30 +0200

    Done with 1.b

--- 2019/tut4/q1.py
+++ 2019/tut4/q1.py
@@ -6,7 +6,7 @@ import scipy.linalg # use numpy if scipy unavailable
 from scipy.spatial import distance
 numpy.set_printoptions(threshold=numpy.inf)
 
-def display_matches():
+def display_matches(matches):
     for match in matches:
         point1 = (int(match[0]),int(match[1]))
         point2 = (int(match[2]), int(match[3]))
@@ -17,8 +17,7 @@ def display_matches():
         #draw line
         cv.line(img, point1, point2, (0,255,0), 2)
 
-def threshold_outliers(threshold=0):
-    global matches
+def threshold_outliers(matches):
     edistances = []
     for match in matches:
         point1 = (int(match[0]),int(match[1]))
@@ -36,7 +35,7 @@ def threshold_outliers(threshold=0):
         if (distance.euclidean(point1, point2) <= upper_bound):
             refined_matches.append(match)
     refined_matches = numpy.asarray(refined_matches)
-    matches = refined_matches
+    return refined_matches
 
 def scale_image(image):
     scale_percent = 35 # percent of original size
@@ -52,20 +51,21 @@ def display_thresholding_difference():
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
     matches = numpy.loadtxt("matches.txt")
-    display_matches()
+    display_matches(matches)
     cv.imwrite("/home/gjsdt/Git_Repositories/compvi-364/2019/tut4/fountain_all_matches.jpg", img)
     img = cv.imread('fountain1.jpg')
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
-    threshold_outliers()
-    display_matches()
+    matches = threshold_outliers(matches)
+    display_matches(matches)
     cv.imwrite("/home/gjsdt/Git_Repositories/compvi-364/2019/tut4/fountain_filtered_matches.jpg", img)
 
-    
-def gen_ransac_fundamental_matrix():
-    matches = numpy.loadtxt("matches.txt")
-    
-    return
+def gen_ransac_fundamental_matrix(data):
+    model = FundamentalMatrixModel();
+    ransac_fit, ransac_data = ransac(data,model,8, 1000, 9, 5,debug=False,return_all=True)
+    final_inlier_data = data[ransac_data[:],:]
+    final_fundamental_matrix = model.fit(final_inlier_data)
+    return final_fundamental_matrix, final_inlier_data;
 
 
 def ransac(data,model,n,k,t,d,debug=False,return_all=False):
@@ -80,8 +80,14 @@ def ransac(data,model,n,k,t,d,debug=False,return_all=False):
         maybemodel = model.fit(maybeinliers)
         test_err = model.get_error( test_points, maybemodel)
         # select indices of rows with accepted points
-        also_idxs = test_idxs[test_err < t]
-        alsoinliers = data[also_idxs,:]
+        also_idxs = []
+        for i in range(len(test_err)):
+            if test_err[i] < t:
+                also_idxs.append(test_idxs[i])
+        also_idxs = numpy.asarray(also_idxs)        
+        #also_idxs = test_idxs[test_err < t]
+        if also_idxs.size != 0:
+            alsoinliers = data[also_idxs,:]
         if debug:
             print 'test_err.min()',test_err.min()
             print 'test_err.max()',test_err.max()
@@ -101,7 +107,8 @@ def ransac(data,model,n,k,t,d,debug=False,return_all=False):
     if bestfit is None:
         raise ValueError("did not meet fit acceptance criteria")
     if return_all:
-        return bestfit, {'inliers':best_inlier_idxs}
+        
+        return bestfit, best_inlier_idxs
     else:
         return bestfit
 
@@ -116,14 +123,14 @@ def random_partition(n,n_data):
 
 class FundamentalMatrixModel:
     
-    def fit(data):
+    def fit(self, data):
         A = numpy.zeros(shape=(data.shape[0],9));
         #construct matrix A -----------------------
         for i in range(data.shape[0]):
-            x_a = data[0];
-            y_a = data[1];
-            x_b = data[2];
-            y_b = data[3];
+            x_a = data[i,0];
+            y_a = data[i,1];
+            x_b = data[i,2];
+            y_b = data[i,3];
             
             A[i,0] = x_a * x_b;
             A[i,1] = y_a * x_b;
@@ -139,170 +146,28 @@ class FundamentalMatrixModel:
         v_A = vh_A.T;
         f = v_A[:,-1]
         F_cap = [[f[0],f[1],f[2]],[f[3],f[4],f[5]],[f[6],f[7],f[8]]];
-        u_F_cap, s_F_cap, vh_F_cap = numpy.linalg.svd(A, full_matrices=True);
+        u_F_cap, s_F_cap, vh_F_cap = numpy.linalg.svd(F_cap, full_matrices=True);
         #force matrix to be of rank 2
         s_F_cap[2] = 0;
         s_mat = numpy.diag(s_F_cap);
         #reconstruct F
-        F = numpy.dot(u_F_cap, numpy.dot(smat, vh_F_cap));
+        F = numpy.dot(u_F_cap, numpy.dot(s_mat, vh_F_cap));
         return F;
         
-    def get_error(data, model):
+    def get_error(self, data, model):
         n_data_points = data.shape[0];
         error_list = numpy.zeros(shape=(n_data_points,1));
         for i in range(n_data_points):
             x_a_vector = numpy.asarray([data[i,0], data[i,1], 1]);
             x_b_vector = numpy.asarray([data[i,2], data[i,3], 1]);
-            
+            F = model;
+            Fxa = numpy.matmul(F,x_a_vector);
+            Ftxb = numpy.matmul(F.T,x_b_vector)
+            numerator = numpy.matmul(x_b_vector.T,numpy.matmul(F,x_a_vector))**2; 
+            denomenator = ((Fxa[0])**2)+((Fxa[1])**2)+((Ftxb[0])**2)+((Ftxb[1])**2);
+            error_list[i] = numerator/denomenator;
         return error_list
 
-class LinearLeastSquaresModel:
-    """linear system solved using linear least squares
-
-    This class serves as an example that fulfills the model interface
-    needed by the ransac() function.
-    
-    """
-    def __init__(self,input_columns,output_columns,debug=False):
-        self.input_columns = input_columns
-        self.output_columns = output_columns
-        self.debug = debug
-    def fit(self, data):
-        A = numpy.vstack([data[:,i] for i in self.input_columns]).T
-        B = numpy.vstack([data[:,i] for i in self.output_columns]).T
-        x,resids,rank,s = scipy.linalg.lstsq(A,B)
-        return x
-    def get_error( self, data, model):
-        A = numpy.vstack([data[:,i] for i in self.input_columns]).T
-        B = numpy.vstack([data[:,i] for i in self.output_columns]).T
-        B_fit = scipy.dot(A,model)
-        err_per_point = numpy.sum((B-B_fit)**2,axis=1) # sum squared error per row
-        return err_per_point
         
-        
-def test():
-    # generate perfect input data
-
-    n_samples = 500
-    n_inputs = 1
-    n_outputs = 1
-    A_exact = 20*numpy.random.random((n_samples,n_inputs) )
-    perfect_fit = 60*numpy.random.normal(size=(n_inputs,n_outputs) ) # the model
-    B_exact = scipy.dot(A_exact,perfect_fit)
-    assert B_exact.shape == (n_samples,n_outputs)
-
-    # add a little gaussian noise (linear least squares alone should handle this well)
-    A_noisy = A_exact + numpy.random.normal(size=A_exact.shape )
-    B_noisy = B_exact + numpy.random.normal(size=B_exact.shape )
-
-    if 1:
-        # add some outliers
-        n_outliers = 100
-        all_idxs = numpy.arange( A_noisy.shape[0] )
-        numpy.random.shuffle(all_idxs)
-        outlier_idxs = all_idxs[:n_outliers]
-        non_outlier_idxs = all_idxs[n_outliers:]
-        A_noisy[outlier_idxs] =  20*numpy.random.random((n_outliers,n_inputs) )
-        B_noisy[outlier_idxs] = 50*numpy.random.normal(size=(n_outliers,n_outputs) )
-
-    # setup model
-
-    all_data = numpy.hstack( (A_noisy,B_noisy) )
-    
-    input_columns = range(n_inputs) # the first columns of the array
-    output_columns = [n_inputs+i for i in range(n_outputs)] # the last columns of the array
-    debug = False
-    model = LinearLeastSquaresModel(input_columns,output_columns,debug=debug)
-
-    linear_fit,resids,rank,s = scipy.linalg.lstsq(all_data[:,input_columns],
-                                                  all_data[:,output_columns])
-
-    # run RANSAC algorithm
-    ransac_fit, ransac_data = ransac(all_data,model,
-                                     50, 1000, 7e3, 300, # misc. parameters
-                                     debug=debug,return_all=True)
-    if 1:
-        import pylab
-
-        sort_idxs = numpy.argsort(A_exact[:,0])
-        A_col0_sorted = A_exact[sort_idxs] # maintain as rank-2 array
-
-        if 1:
-            pylab.plot( A_noisy[:,0], B_noisy[:,0], 'k.', label='data' )
-            pylab.plot( A_noisy[ransac_data['inliers'],0], B_noisy[ransac_data['inliers'],0], 'bx', label='RANSAC data' )
-        else:
-            pylab.plot( A_noisy[non_outlier_idxs,0], B_noisy[non_outlier_idxs,0], 'k.', label='noisy data' )
-            pylab.plot( A_noisy[outlier_idxs,0], B_noisy[outlier_idxs,0], 'r.', label='outlier data' )
-        pylab.plot( A_col0_sorted[:,0],
-                    numpy.dot(A_col0_sorted,ransac_fit)[:,0],
-                    label='RANSAC fit' )
-        pylab.plot( A_col0_sorted[:,0],
-                    numpy.dot(A_col0_sorted,perfect_fit)[:,0],
-                    label='exact system' )
-        pylab.plot( A_col0_sorted[:,0],
-                    numpy.dot(A_col0_sorted,linear_fit)[:,0],
-                    label='linear fit' )
-        pylab.legend()
-        pylab.show()
-
-        
-def gen_2Ddata():
-    # generate perfect input data
-
-    n_samples = 500
-    n_inputs = 1
-    n_outputs = 1
-    A_exact = 20*numpy.random.random((n_samples,n_inputs) )
-    perfect_fit = 60*numpy.random.normal(size=(n_inputs,n_outputs) ) # the model
-    B_exact = scipy.dot(A_exact,perfect_fit)
-    assert B_exact.shape == (n_samples,n_outputs)
-
-    # add a little gaussian noise (linear least squares alone should handle this well)
-    A_noisy = A_exact + numpy.random.normal(size=A_exact.shape )
-    B_noisy = B_exact + numpy.random.normal(size=B_exact.shape )
-
-    if 1:
-        # add some outliers
-        n_outliers = 100
-        all_idxs = numpy.arange( A_noisy.shape[0] )
-        numpy.random.shuffle(all_idxs)
-        outlier_idxs = all_idxs[:n_outliers]
-        non_outlier_idxs = all_idxs[n_outliers:]
-        A_noisy[outlier_idxs] =  20*numpy.random.random((n_outliers,n_inputs) )
-        B_noisy[outlier_idxs] = 50*numpy.random.normal(size=(n_outliers,n_outputs) )
-
-    # setup model
-
-    all_data = numpy.hstack( (A_noisy,B_noisy) )    
-    return all_data;
-
-    
-    
 if __name__=='__main__':
-    '''data = numpy.loadtxt("matches.txt")
-    n = 8;
-    print(data.shape[0]);
-    maybe_idxs, test_idxs = random_partition(n,data.shape[0])
-    print(maybe_idxs);
-    print(test_idxs);
-    print("MAYBE INLIERS\n\n\n")
-    maybeinliers = data[maybe_idxs,:]
-    print(maybeinliers);
-    print("test points\n\n\n")
-    test_points = data[test_idxs]
-    print(test_points);'''
     
-    '''
-    n = 8;
-    data = gen_2Ddata();
-    print(data.shape[0]);
-    maybe_idxs, test_idxs = random_partition(n,data.shape[0])
-    print(maybe_idxs);
-    print(test_idxs);
-    print("MAYBE INLIERS\n\n\n")
-    maybeinliers = data[maybe_idxs,:]
-    print(maybeinliers);
-    print("test points\n\n\n")
-    test_points = data[test_idxs]
-    print(test_points);'''
-    test();
